71eb9d2b09ca681c20b54f6d3fc71f03e088ffcc
Björn Holm <bjh@kth.se>
2018-08-11 17:42:23 +0200

    moveable rectangle
    save .asset as text

---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+    public float maxSpeed = 1;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        float vx = Input.GetAxis("Horizontal");
+        float vy = Input.GetAxis("Vertical");
+
+        Vector2 speed = Vector2.ClampMagnitude(new Vector2(vx, vy), 1) * maxSpeed;
+
+        transform.Translate(speed);
+    }
+}
